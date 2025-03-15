458a1d83fa726d7c5233f5cb1653870bc4b57a48
clarklindev <clarklindev@gmail.com>
2025-03-15 08:26:15 +0800

    starting files - server has been setup and https certs installed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config(); 
 const fs = require('fs');
 const https = require('https')
 const express = require('express');
@@ -20,13 +20,15 @@ const expressServer = https.createServer({key, cert}, app);
 const io = socketio(expressServer,{
     cors: {
         origin: [
-            "https://localhost",
-            // 'https://LOCAL-DEV-IP-HERE' //if using a phone or another computer
+            'https://localhost:8181',
+            'https://192.168.1.103:8181' //if using a phone or another computer
         ],
         methods: ["GET", "POST"]
     }
 });
-expressServer.listen(8181);
+expressServer.listen(8181, ()=>{
+    console.log(`listening on ${process.env.LOCAL_MACHINE_IP}:${process.env.PORT}`)
+});
 
 //offers will contain {}
 const offers = [
